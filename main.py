7eea00883f504722328d13aa475c89c436ea14af
sherryuser <sherryuser1@gmail.com>
2023-11-19 00:53:22 +0500

    Confirmation message fix.

--- main.py
+++ main.py
@@ -127,10 +127,10 @@ def receive_message(message, recipient_id):
         connection.commit()
 
         # Send a confirmation message to the sender
-        bot.send_message(user_id, "<b>✅ Question sent!</b>/n/n"
-                                        f"<b>📨 Your link for questions:</b>\n"
-                                        f"<a href='t.me/{bot.get_me().username}?start={user_id}'>t.me/{bot.get_me().username}?start={user_id}</a>\n\n" \
-                                        "🔐 Send this link to friends and followers to receive anonymous questions!")
+        bot.send_message(user_id, "✅ Question sent!\n\n"
+                                  f"📨 Your link for questions:\n"
+                                  f"t.me/{bot.get_me().username}?start={user_id}\n\n"
+                                  "🔐 Send this link to friends and followers to receive anonymous questions!")
 
         # Send the message to the recipient with a "Reply" button
         send_message_to_recipient(
